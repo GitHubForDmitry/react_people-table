8e32abece252d8ba016a4d02de289c15e7d8e785
GitHubForDmitry <dmitrykoka@gmail.com>
2019-07-14 20:55:08 +0300

    Updates

---
+++ precache-manifest.3b5f0dd1c8a42e08f1ed9b037d9f0d3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3595345a0d383cdd2ea06dda9fea8300",
+    "url": "/react_people-table/index.html"
+  },
+  {
+    "revision": "5653f78b036cd19dd904",
+    "url": "/react_people-table/static/css/main.776b88f7.chunk.css"
+  },
+  {
+    "revision": "6627b6f130d83cb7d827",
+    "url": "/react_people-table/static/js/2.2df16912.chunk.js"
+  },
+  {
+    "revision": "5653f78b036cd19dd904",
+    "url": "/react_people-table/static/js/main.4fc12dbd.chunk.js"
+  },
+  {
+    "revision": "c6b2e506936d419d6d52",
+    "url": "/react_people-table/static/js/runtime~main.b2b73302.js"
+  }
+]);
